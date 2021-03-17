bce5f95feb0681acff2093c69e3c3208befd96ce
Tao Wen <taowen@chengfayun.com>
2021-03-17 08:58:50 +0800

    simplify backend entrypoint

--- src/backend.ts
+++ src/backend.ts
@@ -1,32 +1,16 @@
-import { codegen, Model } from "@rotcare/codegen";
-import * as path from 'path';
+import { codegen, Model } from '@rotcare/codegen';
+import { registerServerless } from '@rotcare/cloud';
+import { generateServerlessFunctions } from '@rotcare/cloud';
+import { Impl } from '@rotcare/io';
 
-export const services = codegen((...models: Model[]) => {
-    const lines = [
-        `
-const { Impl, Scene } = require('@rotcare/io');
-SERVERLESS.ioConf = {
-    database: new Impl.InMemDatabase(),
-    serviceProtocol: undefined,
-};
-SERVERLESS.functions.migrate = new Impl.HttpRpcServer(SERVERLESS, () => import('@motherboard/Migrate'), 'Migrate', 'migrate').handler;`,
-    ];
-    models.sort((a, b) => a.qualifiedName.localeCompare(b.qualifiedName));
-    for (const model of models) {
-        if (model.archetype !== 'ActiveRecord' && model.archetype !== 'Gateway') {
-            continue;
-        }
-        const services = [...model.staticProperties.map(p => p.name), ...model.staticMethods.map(m => m.name)]
-        for (const service of services) {
-            const className = path.basename(model.qualifiedName);
-            lines.push(
-                [
-                    `SERVERLESS.functions.${service} = new Impl.HttpRpcServer(SERVERLESS, `,
-                    `() => import('@motherboard/${model.qualifiedName}'), `,
-                    `'${className}', '${service}').handler;`,
-                ].join(''),
-            );
-        }
-    }
-    return lines.join('\n');
+export const functions = codegen((...models: Model[]) => {
+    return generateServerlessFunctions(models);
+});
+
+registerServerless({
+    ioConf: {
+        database: new Impl.InMemDatabase(),
+        serviceProtocol: undefined as any,
+    },
+    functions,
 });
