28b7e1ba67a33d0aa24c446d3e69d0732313caeb
Tao Wen <taowen@chengfayun.com>
2021-03-19 20:28:52 +0800

    fix demo-rpc

--- demos/demo-rpc/frontend.ts
+++ demos/demo-rpc/frontend.ts
@@ -2,12 +2,13 @@ import { renderRootWidget } from '@rotcare/rx-react';
 import { HomePage } from './Home/Ui/HomePage';
 import { InMemDatabase, Scene } from '@rotcare/io';
 import { HttpRpcClient } from '@rotcare/io-http-rpc';
+import { ActiveRecord } from '@rotcare/active-record';
 
 Scene.currentProject = '@rotcare/demo';
 Scene.serviceDiscover = (options) => {
     return { host: 'localhost', port: options.port }
 }
 renderRootWidget(HomePage, {
-    serviceProtocol: new HttpRpcClient(),
+    serviceProtocol: new HttpRpcClient({ decode: ActiveRecord.decode }),
     database: new InMemDatabase(),
 });
