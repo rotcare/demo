a161a7dd30ed1b8577a675bdbbe1ccb91e63b1bb
Tao Wen <taowen@gmail.com>
2021-03-27 22:06:56 +0800

    fix react demo

--- demos/demo-react/Main/frontend.ts
+++ demos/demo-react/Main/frontend.ts
@@ -1,8 +1,7 @@
 import { renderRootWidget } from '@rotcare/rx-react';
 import { HomePage } from './Ui/HomePage';
-import { InMemDatabase } from '@rotcare/io';
+import { InMemDatabase, ServiceDispatcher } from '@rotcare/io';
 
 renderRootWidget(HomePage, {
-    serviceProtocol: undefined as any,
-    database: new InMemDatabase(),
+    service: new ServiceDispatcher(new InMemDatabase(), undefined as any),
 });
